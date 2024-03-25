2a9d08acd2bc476ec87002f84d08d472750ee826
benjaminpeto <benjaminpeto23@gmail.com>
2024-03-25 07:54:18 +0000

    revoke keys

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,6 @@ import CreateChatButton from "./CreateChatButton";
 async function Header() {
 	const session = await getServerSession(authOptions);
 
-	console.log(session);
-
 	return (
 		<header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
 			<nav className="flex flex-col sm:flex-row items-center p-5 pl-2 max-w-7xl mx-auto">
--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBFvQm5qjjnjlMlfi3fZPyP3QY9NKtXEFk",
-	authDomain: "saas-chat-translation-ap-861ba.firebaseapp.com",
-	projectId: "saas-chat-translation-ap-861ba",
-	storageBucket: "saas-chat-translation-ap-861ba.appspot.com",
-	messagingSenderId: "916293459233",
-	appId: "1:916293459233:web:5382f81d0ac98d71587260",
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
